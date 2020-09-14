0575c3c9eaaf0459a72eb98e30fab79c7ecf3b43
Meenal Phartiyal <69032202+meenalphartiyal@users.noreply.github.com>
2020-09-14 11:48:59 +0530

    Update Program 1.java

--- Java/Program 1.java
+++ Java/Program 1.java
@@ -1,1 +1,11 @@
+import java.io.*;
+class Solution{
+    public static void main(String args[])throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int n1=Integer.parseInt(br.readLine());
+        int n2=Integer.parseInt(br.readLine());
+        int n3=Integer.parseInt(br.readLine());
+        System.out.printf("%d\n%d\n%d",n1,n2,n3);
+    }
+}
 
