f4f2bbcee2875f1f6800551b49ec6d80c3b9cbde
Meenal Phartiyal <69032202+meenalphartiyal@users.noreply.github.com>
2020-09-14 11:36:24 +0530

    Update Program 1.cpp

--- Cpp/Program 1.cpp
+++ Cpp/Program 1.cpp
@@ -1,1 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    int a,b,c;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    cin>>a>>b>>c;
+    cout<<a+b+c; 
+    return 0;
+}
