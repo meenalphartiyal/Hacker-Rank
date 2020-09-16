fa13760c01577f5f10e8da97cc5bf7196c7cdfc7
Meenal Phartiyal <69032202+meenalphartiyal@users.noreply.github.com>
2020-09-16 22:23:35 +0530

    Create Basic Data type.cpp

---
+++ Cpp/Basic Data type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char x;
+    float c;
+    double d;
+    scanf("%d %ld %c %f %lf",&a,&b,&x,&c,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf",a,b,x,c,d);
+    return 0;
+}
